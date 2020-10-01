3de5f31a9a09d02ccb33a3ebf29c0d0059a98811
Jihyo Kim <kimjihyo0325@gmail.com>
2020-10-02 01:16:40 +0900

    some commit

--- comp4537/assignments/index.js
+++ comp4537/assignments/index.js
@@ -195,3 +195,24 @@ const toggleModal = (shouldShowModal) => {
   const modalManager = document.getElementsByClassName('modal')[0];
   modalManager.style['display'] = shouldShowModal ? 'flex' : 'none';
 }
+
+function setCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    var expires = "; expires=" + date.toGMTString();
+  } else var expires = "";
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  console.log(ca);
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
